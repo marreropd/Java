f1e59091f017abc78b25c6ef75e4fd3484537659
marreropd <pablomarrerofernandez1@gmail.com>
2022-06-15 05:13:37 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=0,b=0,i=10;
+
+        for(;i>b;){
+
+
+            a=b++;
+
+        }
+    System.out.println(a);
+    }
+}
